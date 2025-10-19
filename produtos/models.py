df3a2e8cbca45521d032f3914410ea351b3a0164
Pablo-Vini <pablovgramos@gmail.com>
2025-10-19 18:23:58 -0300

    CRUD produtos criado

--- produtos/models.py
+++ produtos/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Produto(models.Model):
+    codigo = models.IntegerField()
+    descricao = models.CharField(max_length=200)
+    valor_unitario = models.DecimalField(max_digits=10, decimal_places=2)
+    data_cadastro = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.nome
--- produtos/views.py
+++ produtos/views.py
@@ -1,4 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Produto
+from datetime import date
 
 def produtos(request):
-    return render(request, 'produtos.html')
+    if request.method == "GET":
+        return render(request, 'produtos.html')
+    elif request.method == "POST":
+        codigo = request.POST.get('codigo')
+        descricao = request.POST.get('descricao')
+        valor_unitario = request.POST.get('valor_unitario')
+
+        produto = Produto.objects.filter(codigo=codigo)
+
+        if produto.exists():
+            return HttpResponse('Usuário já está cadastrado!')
+        else:
+            data = date.today()
+            data_cadastro = data.strftime("%d/%m/%Y")
+            dados_produto = Produto(codigo=codigo, descricao=descricao, valor_unitario=valor_unitario, data_cadastro=data_cadastro)
+            dados_produto.save()
+            return HttpResponse('Cadastro realizado com sucesso!')
