a7c314bee65d5c7169f328b2f8e55bb2f83ebc85
Pablo-Vini <pablovgramos@gmail.com>
2025-10-19 19:04:47 -0300

    CRUD de endereços criado

--- enderecos/models.py
+++ enderecos/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Endereco(models.Model):
+    titulo = models.CharField(max_length=50)
+    cep = models.CharField(max_length=20)
+    logradouro = models.CharField(max_length=50)
+    bairro = models.CharField(max_length=50)
+    cidade = models.CharField(max_length=50)
+    estado = models.CharField(max_length=50)
+    numero = models.IntegerField()
+    complemento = models.CharField(max_length=100)
+    cliente = models.CharField(max_length=14)
+    padrao = models.BooleanField()
+    
+    def __str__(self):
+        return self.nome
--- enderecos/views.py
+++ enderecos/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Endereco
 
 def enderecos(request):
-    return render(request, 'enderecos.html')
+    if request.method == "GET":
+        return render(request, 'enderecos.html')
+    elif request.method == "POST":
+        titulo = request.POST.get('titulo')
+        cep = request.POST.get('cep')
+        logradouro = request.POST.get('logradouro')
+        bairro = request.POST.get('bairro')
+        cidade = request.POST.get('cidade')
+        estado = request.POST.get('estado')
+        numero = request.POST.get('numero')
+        complemento = request.POST.get('complemento')
+        cliente = request.POST.get('cliente')
+        #padrao = request.POST.get('padrao')
+
+        
+        endereco = Endereco.objects.filter(cep=cep)
+
+        if endereco.exists():
+            return HttpResponse('Endereço já está cadastrado!')
+        else:
+            dados_endereco = Endereco(titulo=titulo, cep=cep, logradouro=logradouro, bairro=bairro, cidade=cidade, estado=estado, numero=numero, complemento=complemento, cliente=cliente, padrao=True)
+            dados_endereco.save()
+            return HttpResponse('Cadastro realizado com sucesso!')
